c39d07604fdefbdbb307b6dc861e07d83d15b241
DIY Brad <brad@bradwenner.com>
2025-06-06 23:26:35 -0700

    [fix] Map icon clean up

--- assets/js/leaflet-custom-icons.js
+++ assets/js/leaflet-custom-icons.js
@@ -3,44 +3,60 @@ document.addEventListener('DOMContentLoaded', function() {
     // Define custom icons
     window.customIcons = {
         // DC colored bright flagred
-        flag: L.icon({
+        'flag': L.icon({
             iconUrl: '/assets/images/icons/flag.png',
-            shadowUrl: '/assets/images/icons/flagshadow.png',
+            shadowUrl: '/assets/images/icons/shadow-flag.png',
             iconSize: [56, 60],     // size of the icon
             iconAnchor: [12, 54],   // point of the icon which will correspond to marker's location
-            popupAnchor: [0, -40],  // point from which the popup should open relative to the iconAnchor
             shadowSize: [60, 60],
-            shadowAnchor: [12, 54]
+            shadowAnchor: [12, 54],
         }),
         // Red flag icon
-        flagred: L.icon({
-            iconUrl: '/assets/images/icons/flagred.png',
-            shadowUrl: '/assets/images/icons/flagshadow.png',
+        'flag-red': L.icon({
+            iconUrl: '/assets/images/icons/flag-red.png',
+            shadowUrl: '/assets/images/icons/shadow-flag.png',
             iconSize: [56, 60],     // size of the icon
             iconAnchor: [12, 54],   // point of the icon which will correspond to marker's location
-            popupAnchor: [0, -40,],   // point from which the popup should open relative to the iconAnchor
             shadowSize: [60, 60],
-            shadowAnchor: [12, 54]
+            shadowAnchor: [12, 54],
         }),
         // Yellow flag icon
-        flagyellow: L.icon({
-            iconUrl: '/assets/images/icons/flagyellow.png',
-            shadowUrl: '/assets/images/icons/flagshadow.png',
+        'flag-yellow': L.icon({
+            iconUrl: '/assets/images/icons/flag-yellow.png',
+            shadowUrl: '/assets/images/icons/shadow-flag.png',
             iconSize: [52, 60],     // size of the icon
             iconAnchor: [7, 56],   // point of the icon which will correspond to marker's location
-            popupAnchor: [0, -40],   // point from which the popup should open relative to the iconAnchor
             shadowSize: [60, 60],
-            shadowAnchor: [7, 56]
+            shadowAnchor: [7, 56],
         }),
         // Purple flag icon
-        flagpurple: L.icon({
-            iconUrl: '/assets/images/icons/flagpurple.png',
-            shadowUrl: '/assets/images/icons/flagshadow.png',
+        'flag-purple': L.icon({
+            iconUrl: '/assets/images/icons/flag-purple.png',
+            shadowUrl: '/assets/images/icons/shadow-flag.png',
             iconSize: [48, 60],     // size of the icon
             iconAnchor: [10, 54],   // point of the icon which will correspond to marker's location
-            popupAnchor: [0, -40],   // point from which the popup should open relative to the iconAnchor
             shadowSize: [60, 60],
-            shadowAnchor: [10, 54]
+            shadowAnchor: [10, 54],
+        }),
+        // Circle Map Icon
+        'circle-map': L.icon({
+            iconUrl: '/assets/images/icons/circle-map.png',
+            //shadowUrl: '/assets/images/icons/shadow-flag.png',
+            iconSize: [60, 60],     // size of the icon
+            iconAnchor: [30, 30],   // point of the icon which will correspond to marker's location
+            tooltipAnchor: [30, 0],   // point from which the tooltip should open relative to the iconAnchor
+            //shadowSize: [60, 60],
+            //shadowAnchor: [10, 54]
+        }),
+        // Circle Map Red
+        'circle-map-red': L.icon({
+            iconUrl: '/assets/images/icons/circle-map-red.png',
+            //shadowUrl: '/assets/images/icons/shadow-flag.png',
+            iconSize: [60, 60],     // size of the icon
+            iconAnchor: [30, 30],   // point of the icon which will correspond to marker's location
+            tooltipAnchor: [30, 0],   // point from which the tooltip should open relative to the iconAnchor
+            //shadowSize: [60, 60],
+            //shadowAnchor: [10, 54]
         })
     };
 });
